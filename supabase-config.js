1d8b8bed64e8c767bf2a53767ce805d9e0d1eae4
lerabelka300 <bulgakovaleriia@gmail.com>
2026-06-22 22:55:05 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@
 //  SUPABASE CONFIG — один файл для всего приложения
 // ─────────────────────────────────────────────────────────────
 
-window.SUPABASE_URL  = 'https://hzebqnsuxgcajvkmqejn.supabase.co';
-window.SUPABASE_ANON = 'sb_publishable_4dJd4LLzKL0Lc0bADVFbCA_sS_3Vl9k';
+window.SUPABASE_URL  = 'https://eqwuupjgvpdhdrvpkkek.supabase.co';
+window.SUPABASE_ANON = 'sb_publishable_1QK-kaeL2bMFH27g61ffog_LnktIzGd';
